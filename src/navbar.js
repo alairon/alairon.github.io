f05b2a3f0b09f7e6a638817bf7c71ca884eb0d29
Ambrose <alairon@outlook.com>
2019-07-21 23:23:21 -0400

    Updates

--- src/navbar.js
+++
@@ -1,6 +0,0 @@
-const mainNav = document.getElementById('navbar');
-const navToggle = document.getElementById('navbar-toggle');
-
-navToggle.addEventListener('click', () => {
-    mainNav.classList.toggle('active');
-});
