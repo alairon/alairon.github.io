2fc93c4383775d46dda4a2e090e2131a562c02dc
Ambrose <alairon@outlook.com>
2019-07-25 19:45:31 -0400

    Updated index content

--- src/js/home.js
+++ src/js/home.js
@@ -11,13 +11,20 @@ export default class Home extends React.Component {
         </Helmet>
         <div id="content">
           <h1 id="welcomeHeader">Welcome!</h1>
-          <p id="p-body">
-            Welcome to my website! My name is Ambrose Lai, and I re-created (yes, re-created) this website because I wanted to develop my HTML/CSS skills and to learn React on top of it.
+          <p>
+            Welcome to my website! My name is Ambrose Lai, and I re-created (yes, re-created) this website because I wanted to refine my HTML/CSS skills while learning React.
             <br />
-            My previous experience with HTML/CSS heavily depended on the use of the Bootstrap CSS framework.
-            So when it came to developing this site, I decided to challenge myself to attempt creating a nice-looking website <strong>without</strong> the assistance of the framework. Truth be told, I am quite pleased with the results so far.
+            My previous experience with HTML/CSS heavily relied on the use of the Bootstrap CSS framework.
+            So when it came to developing this site, I decided to challenge myself with three restrictions:
+          </p>
+            <ol>
+              <li>All CSS must be written from scratch &mdash; Bootstrap isn't allowed!</li>
+              <li>The site cannot be built upon an existing template</li>
+              <li>The site must be built with React</li>
+            </ol>
+          <p>
             <br />
-            You may experience glitches, unintended effects, grammatical/spelling mistakes, or anything else throughout the site. In the meantime, please pardon the current state this website is in.
+            I am quite happy with the results so far. However, this site isn't complete or optimized, so you may experience: glitches, unintended effects, or grammatical/spelling mistakes throughout the site. In the meantime, please pardon the current state this website is in.
             <br />
             <br />
             If you would like to see what this site used to look like, <a href={'./legacy/index.html'}>click here to see my legacy home page.</a>
