43215155d90b3e98aa894292fa6880002f5a5545
Ambrose <alairon@outlook.com>
2019-07-27 00:39:07 -0400

    Changed new page load behaviour

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import { Navbar, Footer } from './js/navigation';
 import './css/index.css';
 
+export class ScrollToTopOnMount extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return null;
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
