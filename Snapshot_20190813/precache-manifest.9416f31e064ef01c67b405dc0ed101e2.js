dfe03429922a12f21c61bea273bd3bb773bb42c5
Ambrose Lai <alairon@outlook.com>
2019-08-16 14:02:10 -0400

    Snapshot

---
+++ Snapshot_20190813/precache-manifest.9416f31e064ef01c67b405dc0ed101e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d4a38e93c73e8662fc3baaeb2fd616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8219eb16c01db61c392",
+    "url": "/static/css/main.13af3bd9.chunk.css"
+  },
+  {
+    "revision": "9549a7a8669c476a76ce",
+    "url": "/static/js/2.01f742f2.chunk.js"
+  },
+  {
+    "revision": "a8219eb16c01db61c392",
+    "url": "/static/js/main.4c01c502.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
