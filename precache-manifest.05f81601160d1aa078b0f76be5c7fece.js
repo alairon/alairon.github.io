2f4b3966c4a9619afd54f78ba148b24a55430f4c
Ambrose <alairon@outlook.com>
2019-09-04 15:56:56 -0400

    Updates

---
+++ precache-manifest.05f81601160d1aa078b0f76be5c7fece.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc73fcae15080164bfe582a2b55c92b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa138d4ae4786308076",
+    "url": "/static/css/main.90c65413.chunk.css"
+  },
+  {
+    "revision": "1dd92c010db00c3ec845",
+    "url": "/static/js/2.4b80c366.chunk.js"
+  },
+  {
+    "revision": "caa138d4ae4786308076",
+    "url": "/static/js/main.75b7edff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
